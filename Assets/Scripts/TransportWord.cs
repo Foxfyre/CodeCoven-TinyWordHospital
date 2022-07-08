4cfb709e1727a5b28a51432f984489529b335c14
Foxfyre <foxfyre2204@gmail.com>
2022-07-08 17:38:55 -0600

    Pick up words

--- Assets/Scripts/TransportWord.cs
+++ Assets/Scripts/TransportWord.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TransportWord : MonoBehaviour
 {
-
+    public GameObject wordObject;
     bool canPickup;
     string label;
+    string collisionName;
+    public Text txt;
+    bool hasWord;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,26 @@ public class TransportWord : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && canPickup == true && label == "word")
+        if (Input.GetKeyDown(KeyCode.Space) && canPickup == true && label == "word" && hasWord == false)
         {
             print("YOU CAN PICK UP THE WORD!");
+            //gets the game object information from the object collided into
+            GameObject gO = GameObject.Find(collisionName);
+            txt = gO.GetComponentInChildren<Text>();
+            print(txt.text);
+            //txt.text = wordObject.display;
+
+            //gets the game object of the ambulance
+            GameObject gA = GameObject.Find("Ambulance");
+            GameObject gW = GameObject.Find("WordHolder");
+            Text gAtext = gA.GetComponentInChildren<Text>();
+            gAtext.text = txt.text;
+            gW.GetComponent<SpriteRenderer>().enabled = true;
+            gAtext.enabled = true;
+
+            gO.GetComponent<SpriteRenderer>().enabled = false;
+            gO.GetComponentInChildren<Text>().enabled = false;
+            hasWord = true;
         }
     }
 
@@ -26,8 +47,11 @@ public class TransportWord : MonoBehaviour
     {
         canPickup = true;
         label = collision.gameObject.tag;
-        print("Your are in the space");
-        print(collision.gameObject);
+        //print("Your are in the space");
+        //print(collision.gameObject);
+        collisionName = collision.gameObject.name;
+
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
